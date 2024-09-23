92094b30554bada6907f58a057cfa957d1e4875c
cyi <halu0614@gmail.con>
2024-09-23 15:00:16 +0800

    motion adjust

--- src/components/feature-work/FeatureWork.tsx
+++ src/components/feature-work/FeatureWork.tsx
@@ -1,48 +1,40 @@
 "use client"
 
-import { motion } from "framer-motion"
+import { motion, useInView } from "framer-motion"
 import Wrapper from "../Wrapper"
 
 import FeatureSticky from "./FeatureSticky"
-
+import { textVariants } from "../hero/Hero"
+import { useRef } from "react";
 
 const FeatureWork = () => {
+  const jumpRef = useRef(null);
+  const isInView = useInView(jumpRef, { once: false })
+
   return (
-    <div className="flex flex-col w-full pt-32 md:pt-48 bg-gray-50">
-      <Wrapper>
-        <div className="flex flex-col items-center w-full">
-          <motion.h2 className="font-extrabold text-3xl md:text-5xl py-2"
-            initial={{opacity: 0, y: 50}} 
-            whileInView={{opacity: 1, y: 0}}
-            viewport={{once: false}}
-            transition={{
-              type: "tween",
-              bounce: 0.5,
-              duration: 0.5,
-            }}
+    <div className="flex flex-col w-full md:pt-32 bg-gray-50">
+      <Wrapper className="py-20">
+        <motion.div className="flex flex-col items-center w-full"
+          variants={textVariants}
+          initial="initial"
+          animate={isInView ? "open" : "closed"}
+          ref={jumpRef}
+        >
+          <motion.h2 variants={textVariants} className="font-extrabold text-3xl md:text-5xl py-2"
           >
             Feature Work
           </motion.h2>
 
-          <motion.p className="text-sm text-center mt-4 px-4"
-            initial={{opacity: 0, y: 50}} 
-            whileInView={{opacity: 1, y: 0}}
-            viewport={{once: false}}
-            transition={{
-              type: "tween",
-              bounce: 0.5,
-              duration: 0.5,
-              delay: 0.3,
-            }}
+          <motion.p variants={textVariants} className="text-sm text-center mt-4 px-4"
           >
             Building Web and Mobile applications, with amazing user experiences and function
           </motion.p>
 
           <div className="flex flex-col md:flex-row items-center justify-center gap-2 m-5">
           </div>
-        </div>
+        </motion.div>
       </Wrapper>
-      
+
       <FeatureSticky />
     </div>
   )
